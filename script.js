1ba78291b7c85cab37a53814bc8aaf4accc8860f
Xolelwa <xolelwacekiso@gmail.com>
2025-08-07 12:32:17 +0200

    added changes

--- script.js
+++ script.js
@@ -27,11 +27,13 @@ class Pet {
   //Function to choose a pet
   
   function choosePet(type) {
-    if (type === 'dog') {
-      currentPet = new Pet('Dog', 'Woof! 🐶', 'https://www.pexels.com/photo/gray-and-white-puppy-3299908/');
-    } else if(type === 'cat')
-      currentPet = new Pet('Cat', 'Meow! 🐱', 'https://www.pexels.com/photo/person-putting-sunglasses-on-a-cat-4587958/');
-    else if(type === 'Bird'){
+    if (type === 'Dog') {
+      currentPet = new Pet('Dog', 'Woof! 🐶', 'https://images.pexels.com/photos/3299908/pexels-photo-3299908.jpeg');
+    } 
+    else if(type === 'Cat'){
+      currentPet = new Pet('Cat', 'Meow! 🐱', 'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg');
+    }
+      else if(type === 'Bird'){
       currentPet = new Pet('bird', 'Chirp! 🦜','https://images.pexels.com/photos/987947/pexels-photo-987947.jpeg');
     }
       else if(type === 'Rabbit'){
@@ -63,7 +65,8 @@ class Pet {
   function showFunActions(petName) {
     const actions = petName === 'Dog'
       ? ['Bark', 'Wag Tail', 'Fetch Ball']
-      : ['Meow', 'Purr', 'Chase Mouse'];
+      : ['Meow', 'Purr', 'Chase Mouse']
+      
 
     const actionBox = document.getElementById('actions');
     actionBox.innerHTML = '<strong>Fun Actions:</strong><br>';
